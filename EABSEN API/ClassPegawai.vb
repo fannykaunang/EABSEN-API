4e39a689db4d5756d98038fae0f4fcb57cd17a5a
fanny kaunang <fannykaunang59@yahoo.com>
2019-02-23 04:33:03 +0900

    LAST

--- EABSEN API/ClassPegawai.vb
+++ EABSEN API/ClassPegawai.vb
@@ -7,15 +7,18 @@ Public Class ClassPegawai
     Public Property TEMPAT_LAHIR As String
     Public Property TGL_LAHIR As Date
     Public Property FOTO_FILEPATH As String
+    Public Property NAMA_SKPD As String
 
     Public Sub New()
     End Sub
-    Public Sub New(ByVal USERID As Integer, ByVal NAMA_PEGAWAI As String, ByVal TEMPAT_LAHIR As String, ByVal TGL_LAHIR As Date, ByVal FOTO_FILEPATH As String)
+    Public Sub New(ByVal USERID As Integer, ByVal NAMA_PEGAWAI As String, ByVal TEMPAT_LAHIR As String,
+                   ByVal TGL_LAHIR As Date, ByVal FOTO_FILEPATH As String, NAMA_SKPD As String)
         Me.USERID = USERID
         Me.NAMA_PEGAWAI = NAMA_PEGAWAI & " (" & USERID & ") "
         Me.TEMPAT_LAHIR = TEMPAT_LAHIR
         Me.TGL_LAHIR = TGL_LAHIR
         Me.FOTO_FILEPATH = FOTO_FILEPATH
+        Me.NAMA_SKPD = NAMA_SKPD
     End Sub
 
     Public Function ConvertReader(ByVal reader As SqlDataReader)
@@ -27,7 +30,8 @@ Public Class ClassPegawai
                                      If(IsDBNull(reader.Item("NAMA_PEGAWAI")), Nothing, reader.Item("NAMA_PEGAWAI")),
                                      If(IsDBNull(reader.Item("TEMPAT_LAHIR")), Nothing, reader.Item("TEMPAT_LAHIR")),
                                      If(IsDBNull(reader.Item("TGL_LAHIR")), Nothing, reader.Item("TGL_LAHIR")),
-                                     If(IsDBNull(reader.Item("FOTO_FILEPATH")), Nothing, reader.Item("FOTO_FILEPATH"))))
+                                     If(IsDBNull(reader.Item("FOTO_FILEPATH")), Nothing, reader.Item("FOTO_FILEPATH")),
+                                     If(IsDBNull(reader.Item("NAMA_SKPD")), Nothing, reader.Item("NAMA_SKPD"))))
             End While
         End If
 
@@ -36,7 +40,7 @@ Public Class ClassPegawai
 
     Public Function GetPegawai() As IEnumerable(Of ClassPegawai)
         Dim cn As SqlConnection = New SqlConnection(ConfigurationManager.ConnectionStrings("EABSEN_API.My.MySettings.ConnString").ConnectionString)
-        Dim cmd As New SqlCommand("SELECT * FROM T_PEGAWAI", cn)
+        Dim cmd As New SqlCommand("SELECT * FROM VW_PEGAWAI", cn)
         cmd.CommandType = CommandType.Text
         cn.Open()
         Dim reader As SqlDataReader = cmd.ExecuteReader()
@@ -47,7 +51,7 @@ Public Class ClassPegawai
 
     Public Function GetPegawaiByID(id As Integer) As ClassPegawai
         Dim cn As SqlConnection = New SqlConnection(ConfigurationManager.ConnectionStrings("EABSEN_API.My.MySettings.ConnString").ConnectionString)
-        Dim cmd As New SqlCommand("SELECT * FROM T_PEGAWAI WHERE USERID = @USERID", cn)
+        Dim cmd As New SqlCommand("SELECT * FROM VW_PEGAWAI WHERE USERID = @USERID", cn)
         cmd.CommandType = CommandType.Text
         cmd.Parameters.AddWithValue("@USERID", id)
         cn.Open()
